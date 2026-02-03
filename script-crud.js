5992a7d35e2d3d36669a80207cf96b359717f23e
nicodemoos <victor.nico1@hotmail.com.br>
2026-02-03 15:27:09 -0300

    adicionando no botão ao clicar, aparecer a lista de tarefas

---
+++ script-crud.js
@@ -0,0 +1,9 @@
+const btnAdicionarTarefa = document.querySelector(".app__button--add-task");
+const formAdicionarTarefa = document.querySelector('.app__form-add-task')
+
+btnAdicionarTarefa.addEventListener('click',() =>{
+    formAdicionarTarefa.classList.toggle('hidden')
+});
+
+// O código acima está adicionando no botão ao clicar, aparecer a lista de tarefas
+
